cb6c1875585b4caaffb315d54d8735025f6a2cfb
Balde641 <62099893+Balde641@users.noreply.github.com>
2022-08-24 14:32:51 +0300

    comment

--- hei.py
+++ hei.py
@@ -1,8 +1,3 @@
 
 # Ohjelma, joka tervehtii minua tulostamalla nimeni.
 print("Hei, Mamadou Balde!")
-
-# Program to convert grams to kilograms.
-grams = float(input("Please enter grams: "))
-kilograms = grams / 1000
-print(kilograms, " Kilograms")
--- mod01-examples/example03.py
+++ mod01-examples/example03.py
@@ -5,3 +5,8 @@ random_number = random.randint(1, 9)
 random_number2 = random.randint(1, 9)
 print(f"Arvottu numero välillä 1-9: {random_number}")
 print(f"Arvottu numero välillä 1-9: {random_number2}")
+
+# Program to convert grams to kilograms.
+grams = float(input("Please enter grams: "))
+kilograms = grams / 1000
+print(kilograms, " Kilograms")
