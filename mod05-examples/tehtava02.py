28a611412fa03ccdee08de7cc016cb37e6aff8a2
Balde641 <62099893+Balde641@users.noreply.github.com>
2022-09-09 07:49:47 +0300

    comment

--- mod05-examples/tehtava02.py
+++ mod05-examples/tehtava02.py
@@ -8,8 +8,12 @@ luvut = []
 luku = input("Anna ensimmäinen luku tai lopeta painamalla Enter: ")
 while luku != "":
     luvut.append(luku)
-    luvut.sort(reverse=True)
+    #luvut.sort(reverse=True)
     luku = input("Anna seuraava luku tai lopeta painamalla Enter: ")
 
 for n in luvut:
     print(f"{n}")
+
+K = n
+num = 5
+print('K %s.' % (sorted(K, reverse=True)[:num]))
